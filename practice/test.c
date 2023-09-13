9fc52c0f0ffe7c8e55cf9f9aeb1bca20336bbc82
Yassinean <yassinedubraska@gmail.com>
2023-09-13 13:59:13 +0100

    simple programs in C

--- practice/test.c
+++ practice/test.c
@@ -2,15 +2,86 @@
 #include <stdlib.h>
 #include <math.h>
 int main() {
-    //int x = 21;
-    int x;
-    int y;
-    printf("Bonjour, Saisis un nbre : ");
-    scanf("%d",&x);
-    printf("Saisis le second nbre : ");
-    scanf("%d",&y);
-    int z = pow (x,y);
-    printf ("La puissance est : %d", z);
+  
+  /*----------- La division ----------------*/
+  //  float a ,b ,c;
+  //  printf("Veuillez saisi le 1er nbr :");
+  //  scanf("%f",&a);
+  //  printf("Veuillez saisi le 2eme nbr :");
+  //  scanf("%f",&b);
+  //  c = a /b ;
+  //  if (b != 0)
+  //  {
+  //     printf("La division est: %.2f", c);    
+  //  }else{
+  //     printf("La division sur 0 est impossible");
+  //  }
 
+  /*------ Calcule de chiffre d'affaire -------*/ 
+    // int prduits_vendu;
+    // float prix_vente , CA;
+    // printf("Veuillez entrer le nombre des produits vendus:");
+    // scanf("%d",&prduits_vendu);
+    // printf("Entrer le prix de vente :");
+    // scanf("%f",&prix_vente);
+    // CA = prduits_vendu * prix_vente ;
+    // printf("Le chiffre d'affaires est : %.2f", CA);
+
+    /*--------- Le max des nombres -----------*/
+    // float a , b , max ;
+    // printf("Saisir le nbre 1 :");
+    // scanf("%f",&a);
+    // printf("Saisir le nbre 2 :");
+    // scanf("%f",&b);
+    // max = a ;
+    // if(b > max ){
+    //   printf("le max est : %f",b);
+    // }else printf("le max est : %f",a);
+  /*------- L'etat de liquide -----------------*/
+  // int x;
+  // printf("Entrer la temperature de liquide : ");
+  // scanf("%d",&x);
+  // if(x < 0)
+  //   printf("L'etat du liquide est GLACE");
+  // else if (x > 0 && x < 100)
+  // {
+  //   printf("L'etat du liquide est liquide");
+  // }
+  // else 
+  //   printf("L'etat du liquide est vapeur");
+    
+    /*------- Le nom du jour -------------*/
+    // int j ;
+    // printf("Entrer un nombre entre 1 et 7 :");
+    // scanf("%d",&j);
+    // switch (j)
+    // {
+    // case 1:
+    //   printf("Lundi");
+    // break;
+    // case 2:
+    //   printf("Madi");
+    // break;
+    // case 3:
+    //   printf("Mercredi");
+    // break;
+    // case 4:
+    //   printf("Jeudi");
+    // break;
+    // case 5:
+    //   printf("Vendredi");
+    // break;
+    // case 6:
+    //   printf("Samedi");
+    // break;
+    // case 7:
+    //   printf("Dimanche");
+    // break;
+    
+    // default: printf("J'ai dis un nbre entre 1 et 7");
+    //   break;
+    // }
+
+    
   return 0;
 }
