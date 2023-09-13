fe9e9868e9764d57f4469288f080d6bfd4f136fd
Yassinean <yassinedubraska@gmail.com>
2023-09-13 09:24:05 +0100

    test

---
+++ practice/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main() {
+    //int x = 21;
+    int x;
+    int y;
+    int z = pow (x,y);
+    printf("Bonjour, Saisis un nbre : ");
+    scanf("%d",&x);
+    printf("Saisis le second nbre :");
+    scanf("%d",&y);
+    printf("La puissance est : %d", z);
+
+  return 0;
+}
