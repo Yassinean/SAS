a7c41a3b8f50892ad784c22eecb77dbd7e943554
Yassinean <yassinedubraska@gmail.com>
2023-09-19 11:00:08 +0100

    Challenges SAS

--- Arrays/tri.c
+++ Arrays/tri.c
@@ -3,39 +3,42 @@
 
 int main()
 {
-    // int T[50];
-    // int n,i,echo;
-    // printf("Veuillez taper la dimension du tableau : ");
-    // scanf("%d",&n);
+    /*------------ Tri a bulle ------------*/
+    /*int T[50];
+    int n,i,echo;
+    printf("Veuillez taper la dimension du tableau : ");
+    scanf("%d",&n);
 
-    // printf("Avant le tri : \n");
-    // for ( i = 0; i < n; i++)
-    // {
-    //     printf("Entrer l'element %d: ",i+1);
-    //     scanf("%d",&T[i]);
-    // }
-    // printf("Apres le tri :\t");
-    // do
-    // {
-    //     echo = 0;
-    //     for (i = 0; i < n-1; i++)
-    //     {
-    //         if (T[i]>T[i+1])
-    //         {
-    //             int tmp = T[i];
-    //             T[i] = T[i+1];
-    //             T[i+1]  = tmp;
-    //             echo ++;
-    //         }
+    printf("Avant le tri : \n");
+    for ( i = 0; i < n; i++)
+    {
+        printf("Entrer l'element %d: ",i+1);
+        scanf("%d",&T[i]);
+    }
+    printf("Apres le tri :\t");
+    do
+    {
+        echo = 0;
+        for (i = 0; i < n-1; i++)
+        {
+            if (T[i]>T[i+1])
+            {
+                int tmp = T[i];
+                T[i] = T[i+1];
+                T[i+1]  = tmp;
+                echo ++;
+            }
 
-    //     }
+        }
 
-    // } while (echo > 0);
-    // for ( i = 0; i < n; i++)
-    // {
-    //     printf("%4d ",T[i]);
-    // }
-    int T[50] ;
+    } while (echo > 0);
+    for ( i = 0; i < n; i++)
+    {
+        printf("%4d ",T[i]);
+    }*/
+
+    /*------------ tri par selection ------------*/
+    /*int T[50] ;
     int i, temp, n , j;
 
     printf("Enter the number of cases :");
@@ -63,5 +66,35 @@ int main()
     for (i = 0; i < n; i++)
     {
         printf("%d  \t", T[i]);
+    }*/
+    /*------------ tri par insertion ------------*/
+
+    int T[50];
+    int i, n, temp ,j;
+
+    printf("Enter the number of elements in the table : ");
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++){
+        printf("Enter the element %d : ", i+1);
+        scanf("%d", &T[i]);
     }
+    for ( i = 1; i < n; i++)
+    {
+        temp = T[i];
+        j = i - 1;
+        while (temp < T[j] && j >= 0)
+        {
+            T[j+1] = T[j];
+            j--;
+        }
+        T[j+1] = temp;
+        
+    }
+    printf("The new table is:");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d  \t", T[i]);
+    }
+    
 }
